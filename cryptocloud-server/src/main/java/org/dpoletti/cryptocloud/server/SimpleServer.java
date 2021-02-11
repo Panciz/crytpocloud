2441e433338f5e1a81e81575c8189ee4c82512af
Davide Poletti <dav.poletti@gmail.com>
2021-02-11 15:24:40 +0100

    docker plugin and creating dest dir

--- cryptocloud-server/src/main/java/org/dpoletti/cryptocloud/server/SimpleServer.java
+++ cryptocloud-server/src/main/java/org/dpoletti/cryptocloud/server/SimpleServer.java
@@ -1,5 +1,7 @@
 package org.dpoletti.cryptocloud.server;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.rmi.ServerException;
@@ -12,10 +14,12 @@ public class SimpleServer {
 	private static final Logger logger 
 	  = LoggerFactory.getLogger(SimpleServer.class);
 	
-	public static final void main(String[] args) throws ServerException {
+	public static final void main(String[] args) throws IOException {
 		
 		
 		Path destDir = Paths.get(args[0]);
+		Files.createDirectories(destDir);
+		
 		//TODO dest validation
 		logger.info("FileServer Listen to port 9000 destDir "+destDir);
 
