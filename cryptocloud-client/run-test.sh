1b8aa9d2c0b236779726d5c3cfa80a3e3703d134
Davide Poletti <dav.poletti@gmail.com>
2021-02-13 23:44:03 +0100

    fixed run test script

--- cryptocloud-client/run-test.sh
+++ cryptocloud-client/run-test.sh
@@ -27,7 +27,7 @@ echo "Sending test  encrypt"
 java -cp target/client-0.0.1-SNAPSHOT.jar:target/bcpkix-jdk15on.jar:target/bcprov-jdk15on.jar org.dpoletti.cryptocloud.client.CryptoCloudSimpleClient PUT testdir/sendf/test-image.jpg user 127.0.0.1 9000 testdir/enc.key
 
 echo "Retrive test  encrypt"
-java -cp target/client-0.0.1-SNAPSHOT.jar:target/bcpkix-jdk15on.jar:target/bcprov-jdk15on.jar org.dpoletti.cryptocloud.client.CryptoCloudSimpleClient GET testdir/recf/test-image.jpg dpoletti 127.0.0.1 9000 testdir/enc.key
+java -cp target/client-0.0.1-SNAPSHOT.jar:target/bcpkix-jdk15on.jar:target/bcprov-jdk15on.jar org.dpoletti.cryptocloud.client.CryptoCloudSimpleClient GET testdir/recf/test-image.jpg user 127.0.0.1 9000 testdir/enc.key
 
 echo "Cheching  encrypt result"
 md5sum -c testdir/recf/test-image.md5
