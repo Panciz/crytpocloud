46793b0fcb41ad4563650dd746c433a03a7654ac
Davide Poletti <dav.poletti@gmail.com>
2021-02-14 03:17:46 +0100

    get file list

--- cryptocloud-springclient/src/main/java/org/dpoletti/crytpocloud/springclient/CryptoClientController.java
+++ cryptocloud-springclient/src/main/java/org/dpoletti/crytpocloud/springclient/CryptoClientController.java
@@ -1,15 +1,16 @@
 package org.dpoletti.crytpocloud.springclient;
 
-import javax.websocket.server.PathParam;
-
 import org.dpoletti.cryptocloud.core.exeption.ProviderGenerationException;
 import org.dpoletti.cryptocloud.core.model.OperationType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CryptoClientController {
@@ -17,6 +18,11 @@ public class CryptoClientController {
 	@Autowired
 	ClientWrapper clientWrapper;
 	
+	@Value("${cryptoserver.host}")
+	private String host;
+
+	@Value("${cryptoserver.restport}")
+	private int port;
 	
 	@PutMapping
 	public void postCommand(@RequestBody CryptoCommand operation) throws ProviderGenerationException {
@@ -27,4 +33,18 @@ public class CryptoClientController {
 		clientWrapper.executeOperation(OperationType.GET, operation.getUsername(), operation.getFilename());
 	}
 	
+	@GetMapping(path =  "/{username}")
+	public  UserFile[] getList(@PathVariable(value = "username") String username)
+	{
+	    final String uri = "http://"+host+":"+port+"/"+username;
+	    RestTemplate restTemplate = new RestTemplate();
+	     
+	    ResponseEntity<UserFile[]> response =
+	    		  restTemplate.getForEntity(
+	    				  uri,
+	    				  UserFile[].class);
+	    return response.getBody();
+	     
+	   
+	}
 }
---
+++ cryptocloud-springclient/src/main/java/org/dpoletti/crytpocloud/springclient/UserFile.java
@@ -0,0 +1,50 @@
+package org.dpoletti.crytpocloud.springclient;
+
+
+import java.util.Date;
+
+
+public class UserFile {
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public Date getLastupdate() {
+		return lastupdate;
+	}
+
+	public void setLastupdate(Date lastupdate) {
+		this.lastupdate = lastupdate;
+	}
+
+
+	private Long id;
+		
+	private String username;
+	
+	private String filename;
+	
+	private Date lastupdate;
+
+}
